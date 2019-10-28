8979f44ca915f46ce3ac92449d12c22cb5516e18
sunjones0404 <david@sunjones.com>
2019-10-28 20:06:56 +0000

    Create Bootcamp Mongoose Schema

--- server.js
+++ server.js
@@ -1,17 +1,48 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Load Environment Variables
 dotenv.config({ path: './config/config.env' });
 
+// Connect to DB
+connectDB();
+
+// Import Route Files
+const bootcamps = require('./routes/bootcamps');
+
 // Initialist App
 const app = express();
 
-// PORT
+// Dev Logging Middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Mount Routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+// -------------------------------------
+//  SERVER SETTINGS
+// -------------------------------------
+
+// Port Environment Variable
 const PORT = process.env.PORT || 5000;
 
 // Run Server
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port: ${PORT}`)
+  console.log(
+    ` Server running in ${process.env.NODE_ENV} mode on port: ${PORT} `.black
+      .bgBrightGreen
+  )
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(` Error: ${err.message} `.black.bgBrightRed);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
